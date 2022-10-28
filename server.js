2a000a4dac9c99e37c9e73c8152b5f2bad6e5f1c
Dhairya <dbrahm11@my.centennialcollege.ca>
2022-10-28 15:39:59 -0400

    y

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
  * Module dependencies.
  */
 
-// var configDB = require('./config/db');
+var configDB = require('./config/db');
 var app = require('./config/app');
 var debug = require('debug')('comp228-assignment1:server');
 var http = require('http');
@@ -13,7 +13,7 @@ var passportConfig = require('./config/passport');
  * Get port from environment and store in Express.
  */
 
-// var db = configDB();
+var db = configDB();
 var port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
